25dfc489667e22908ec23dbae3a886e3cb8db6b9
donatali <donatali@umich.edu>
2026-03-22 02:44:21 -0400

    hi

--- game-mode.py
+++ game-mode.py
@@ -13,40 +13,6 @@ import random
 # user points to the object they think it is
 # tts confirms/denies
 
-# -----------------------
-# MediaPipe setup
-# -----------------------
-mp_hands = mp.solutions.hands
-mp_draw = mp.solutions.drawing_utils
-
-hands = mp_hands.Hands(
-    static_image_mode=False,
-    max_num_hands=1,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
-)
-
-# -----------------------
-# YOLO Model
-# -----------------------
-model = YOLO("yolov8s-oiv7.pt")
-
-ignore_classes = ["Building", "Office building", "Clothing"]
-
-label_map = {
-    "Human hand": "Hand",
-    "Human face": "Face",
-    "Footwear": "Shoe",
-    "Desk": "Table",
-    "Tablet computer": "Tablet",
-}
-
-# -----------------------
-# Webcam
-# -----------------------
-cap = cv2.VideoCapture(0)
-
-lang = 'en'
 
 def speak(text, language):
     mp3_fo = BytesIO()
@@ -61,29 +27,128 @@ def speak(text, language):
     while pygame.mixer.get_busy():
         time.sleep(0.1)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
 
-    h, w, _ = frame.shape
+def game():
+    # -----------------------
+    # MediaPipe setup
+    # -----------------------
+    mp_hands = mp.solutions.hands
+    mp_draw = mp.solutions.drawing_utils
 
-     # Convert to RGB for MediaPipe
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    hand_results = hands.process(rgb)
+    hands = mp_hands.Hands(
+        static_image_mode=False,
+        max_num_hands=1,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    )
 
-    yolo_results = model(frame, verbose=False)[0] #YOLO results
+    # -----------------------
+    # YOLO Model
+    # -----------------------
+    model = YOLO("yolov8s-oiv7.pt")
 
-    detected_objs = []
+    ignore_classes = ["Building", "Office building", "Clothing"]
 
-    for box in yolo_results.boxes:
-        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
-        cls = int(box.cls[0])
-        label = model.names[cls]
-        if label not in detected_objs and label not in ignore_classes:
-            detected_objs.append(box)
+    label_map = {
+        "Human hand": "Hand",
+        "Human face": "Face",
+        "Footwear": "Shoe",
+        "Desk": "Table",
+        "Tablet computer": "Tablet",
+    }
+
+    # -----------------------
+    # Webcam
+    # -----------------------
+    cap = cv2.VideoCapture(0)
+
+    lang = 'en'
+    #state machine var
+    # 0: no obj selected
+    # 1: obj selected, waiting for user to get correct
+    # 2: user correct, reset
+    state = 0
+    random_obj = ""
+    timeout = 10
+    timeout_start = 0.0
+
+    while True:
+        start_time = time.perf_counter()
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        h, w, _ = frame.shape
+
+        # Convert to RGB for MediaPipe
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        hand_results = hands.process(rgb)
+
         
-    random_obj = random.choice(detected_objs)
-    
+        if state == 0:
+            yolo_results = model(frame, verbose=False)[0] #YOLO results
+
+            detected_objs = []
+
+            for box in yolo_results.boxes:
+                x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+                cls = int(box.cls[0])
+                label = model.names[cls]
+                if label not in detected_objs and label not in ignore_classes:
+
+                    detected_objs.append(label)
+            
+            # CHANGE THIS CODE TO GET TRANSLATED WORD FROM LABEL!
+            if len(detected_objs) > 0:
+                random_obj = random.choice(detected_objs)
+                word_to_trans = label_map.get(random_obj, random_obj)
+                speak(word_to_trans, lang)
+                timeout_start = time.perf_counter()
+                state = 1
+
+        elif state == 1:
+            if hand_results.multi_hand_landmarks:
+                hand_landmarks = hand_results.multi_hand_landmarks[0]
+
+                tip = hand_landmarks.landmark[8]
+                tip_x = int(tip.x * w)
+                tip_y = int(tip.y * h)
+
+                yolo_results = model(frame, verbose=False)[0] #YOLO results
+
+                for box in yolo_results.boxes:
+                    x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+                    cls = int(box.cls[0])
+                    label = model.names[cls]
+                    if (x1 <= tip_x <= x2 and y1 <= tip_y <= y2) and label == random_obj:
+                        speak("Correct!", 'en')
+                        state = 2
+                        break
+                    # elif (x1 <= tip_x <= x2 and y1 <= tip_y <= y2) and label != random_obj:
+                    #     speak("Try Again!", 'en')
+            if state != 2:
+                current_time = time.perf_counter()
+                if (current_time - timeout_start) > timeout:
+                    speak("Next item", 'en')
+                    state = 2
+
+        elif state == 2:
+            random_obj = ""
+            state = 0
 
 
+        cv2.imshow("LanGo", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+        current_time = time.perf_counter()
+        while (current_time - start_time) < 0.2:
+            time.sleep(0.01)
+            current_time = time.perf_counter()
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    game()
